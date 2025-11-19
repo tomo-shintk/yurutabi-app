caf2dc7a24d25747ab8c2a034f9042e7abb19545
Taishiro Bando <6051_2023_05525@g.miyazaki-c.ed.jp>
2025-11-19 22:07:05 +0000

    randamu-zissou

--- static/script.js
+++ static/script.js
@@ -16,10 +16,27 @@ const swiper = new Swiper('.swiper', {
 
 function goToRandomMap() {
   const maps = [
-    'https://tomo-shintk.github.io/yurutabi-site/oseania.html',
-    'https://tomo-shintk.github.io/yurutabi-site/europe.html',
-    'https://tomo-shintk.github.io/yurutabi-site/asia.html',
-    'https://tomo-shintk.github.io/yurutabi-site/northamerica.html'
+    'https://tomo-shintk.github.io/yurutabi-site/herusinki.html',
+    'https://tomo-shintk.github.io/yurutabi-site/ke-putaun.html',
+    'https://tomo-shintk.github.io/yurutabi-site/matyupityu.html',
+    'https://tomo-shintk.github.io/yurutabi-site/monsan.html',
+    'https://tomo-shintk.github.io/yurutabi-site/amusuterudamu.html',
+    'https://tomo-shintk.github.io/yurutabi-site/berugi.html',
+    'https://tomo-shintk.github.io/yurutabi-site/doitu.html',
+    'https://tomo-shintk.github.io/yurutabi-site/igirisu.html',
+    'https://tomo-shintk.github.io/yurutabi-site/ke-putaun.html',
+    'https://tomo-shintk.github.io/yurutabi-site/kyoto.html',
+    'https://tomo-shintk.github.io/yurutabi-site/meruborun.html',
+    'https://tomo-shintk.github.io/yurutabi-site/naiagara.html',
+    'https://tomo-shintk.github.io/yurutabi-site/piramido.html',
+    'https://tomo-shintk.github.io/yurutabi-site/riodejaneiro.html',
+    'https://tomo-shintk.github.io/yurutabi-site/santori.html',
+    'https://tomo-shintk.github.io/yurutabi-site/rutserun.html',
+    'https://tomo-shintk.github.io/yurutabi-site/sidoni.html',
+    'https://tomo-shintk.github.io/yurutabi-site/supein.html',
+    'https://tomo-shintk.github.io/yurutabi-site/sutokuhorumu.html',
+    'https://tomo-shintk.github.io/yurutabi-site/tai.html',
+    'https://tomo-shintk.github.io/yurutabi-site/venetsia.html',
   ];
   const randomUrl = maps[Math.floor(Math.random() * maps.length)];
   window.open(randomUrl, '_blank');
